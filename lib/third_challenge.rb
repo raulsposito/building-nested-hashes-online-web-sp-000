6caf17a5a6659e52c10d52eadbea47c0ea757aa0
Raul Sposito <raulsposito@gmail.com>
2019-07-29 15:03:43 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,7 +18,7 @@ def third_challenge
         {name: "Benvolio", age: "17", attitude: "worried"},
         {name: "Mercutio", age: "18", attitude: "hot-headed"}
         ]
-   }, 
+   },
    :capulet => {
       :patriarch => {
         name: "Lord Capulet",
